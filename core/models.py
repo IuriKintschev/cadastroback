8dda05e3d9e05ed2b6bbb0318dc965b3011e1f31
IuriKintschev <jtc.iuri07@gmail.com>
2019-05-01 16:29:20 -0400

    model criado e feito migração

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Name(models.Model):
+	nome = models.CharField(max_length=80)
+	telefone = models.CharField(max_length=22)
+	email = models.TextField()
+	
+	def __str__(self):
+		return self.nome
