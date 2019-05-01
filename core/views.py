2277f1c242eedac12a1a1463d5c81823b77de801
IuriKintschev <jtc.iuri07@gmail.com>
2019-05-01 16:42:56 -0400

    cirado o metodo get-all (o django ja prove um crud completo por isso nao implementei)

--- core/views.py
+++ core/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Name
+from .serializers import NameSerializer
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Name.objects.all()
+    serializer_class = NameSerializer
