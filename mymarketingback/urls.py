35df377606701d6149a4acee5448270989fa4c7c
IuriKintschev <jtc.iuri07@gmail.com>
2019-05-01 16:47:27 -0400

    rota criada

--- mymarketingback/urls.py
+++ mymarketingback/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.views import NameViewSet
+from rest_framework import routers
+
+# rotas
+
+router = routers.DefaultRouter()
+router.register('usuarios', NameViewSet)
+
+# rotas end
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
